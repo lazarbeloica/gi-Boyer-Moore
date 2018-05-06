f0092fb3aa8df2b32c0c6fcba808ac59b6d760db
Milan Zafirovic <zafirovicmilan2@gmail.com>
2018-05-06 22:47:10 +0200

    added abstract class Heuristic

--- src/heuristics/heuristic.py
+++ src/heuristics/heuristic.py
@@ -1,3 +1,20 @@
-'''
-Abstract class, used for preprocessing, will be placed in this module.
-'''
+from abc import ABC, abstractmethod
+
+NUMBER_OF_CHARACTERS = 256 #number of characters in alphabet
+
+class Heuristic(ABC):
+
+
+
+    @abstractmethod
+    def get_shift_pattern_found(self): #TODO add arguments for this method
+        pass
+
+    @abstractmethod
+    def get_shift_pattern_not_found(self): #TODO add arguments for this method
+        pass
+
+    @abstractmethod
+    def preprocess(self):
+        pass
+    
