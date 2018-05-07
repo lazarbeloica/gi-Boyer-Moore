0ca3007856f277936866b2d7c756969b0515d7b6
Milan Zafirovic <zafirovicmilan2@gmail.com>
2018-05-07 21:20:29 +0200

    add implementations for abstract methods of BadCharacter class

--- src/heuristics/badcharacter.py
+++ src/heuristics/badcharacter.py
@@ -11,8 +11,14 @@ class BadCharacter(Heuristic):
     def _get_bad_chars(self):
         return self._bad_chars
     
-    def get_shift_pattern_found(self, **kwargs):    #TODO add implementation
-        pass                                    
-    
-    def get_shift_pattern_not_found(self, **kwargs): #TODO add implementation 
-        pass
+    def get_shift_pattern_found(self, **kwargs):    #TODO do a doublecheck
+        text = kwargs['text']
+        text_len = kwargs['text_len']
+        shift = kwargs['shift']
+        return (text_len - self._get_bad_chars()[ord(text[shift + text_len])] if shift + text_len < len(self.get_pattern()) else 1)
+        
+    def get_shift_pattern_not_found(self, **kwargs): #TODO do a doublecheck
+        index = kwargs['index']
+        text = kwargs['text']
+        shift = kwargs['shift'] 
+        return max(1 , index - self._get_bad_chars()[ord(text[shift + index])])
