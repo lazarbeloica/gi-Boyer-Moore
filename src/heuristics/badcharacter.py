c1b854b53e21274d89ef01aa33b4664684c24958
Lazar Beloica <lazarbeloica@gmail.com>
2018-05-11 21:23:46 +0200

    A minor change in the design

--- src/heuristics/badcharacter.py
+++ src/heuristics/badcharacter.py
@@ -1,24 +1,23 @@
 from heuristics.heuristic import Heuristic, NUMBER_OF_CHARACTERS
 
 class BadCharacter(Heuristic):
-    
 
-    def preprocess(self):
+    def preprocess(self, pattern_):
         self._bad_chars = [-1]*NUMBER_OF_CHARACTERS
-        for i in range(len(self.get_pattern())):
-            self.get_pattern()[ord(self.get_pattern()[i])] = i
+        for i in range(len(pattern_):
+            pattern_[ord(pattern_[i])] = i
 
     def _get_bad_chars(self):
         return self._bad_chars
-    
+
     def get_shift_pattern_found(self, **kwargs):    #TODO do a doublecheck
         text = kwargs['text']
         text_len = kwargs['text_len']
         shift = kwargs['shift']
         return (text_len - self._get_bad_chars()[ord(text[shift + text_len])] if shift + text_len < len(self.get_pattern()) else 1)
-        
+
     def get_shift_pattern_not_found(self, **kwargs): #TODO do a doublecheck
         index = kwargs['index']
         text = kwargs['text']
-        shift = kwargs['shift'] 
+        shift = kwargs['shift']
         return max(1 , index - self._get_bad_chars()[ord(text[shift + index])])
--- src/heuristics/compositeheuristic.py
+++ src/heuristics/compositeheuristic.py
@@ -5,24 +5,23 @@ class CompositeHeuristic(Heuristic):
     def __init__(self, *argv):
         self._heuristics = set()
         for arg in argv:
-            self.add(arg)        
+            self.add(arg)
 
-    def preprocess(self):
+    def preprocess(self, pattern_):
         for heuristic in self.get_heuristics():
-            heuristic.preprocess()
-            
+            heuristic.preprocess(pattern_)
+
     def add(self, heuristic):
         self._heuristics.add(heuristic)
-        
+
     def get_heuristics(self):
         return self._heuristics
 
     def remove(self, heuristic):
         self._heuristics.discard(heuristic)
-        
-    def get_shift_pattern_not_found(self, **kwargs): 
-        return max([heuristic.get_shift_pattern_not_found(kwargs) for heuristic in self.get_heuristics()]) #TODO double check                                
-        
-    def get_shift_pattern_found(self, **kwargs): 
+
+    def get_shift_pattern_not_found(self, **kwargs):
+        return max([heuristic.get_shift_pattern_not_found(kwargs) for heuristic in self.get_heuristics()]) #TODO double check
+
+    def get_shift_pattern_found(self, **kwargs):
         return max([heuristic.get_shift_pattern_found(kwargs) for heuristic in self.get_heuristics()]) #TODO double check
-    
