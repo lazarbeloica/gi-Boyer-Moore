b6e4b7242640497d434f21b65f0f1f0dc8a924dd
Lazar Beloica <lazarbeloica@gmail.com>
2018-05-22 01:20:58 +0200

    Preparing code for the test-run on server

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-from src.heuristics.badcharacter import BadCharacter
-from src.heuristics.goodsuffix import GoodSuffix
-from src.heuristics.compositeheuristic import CompositeHeuristic
-from src.performance.performanceanalyser import PerformanceAnalyser
+from heuristics.compositeheuristic import CompositeHeuristic
+from heuristics.badcharacter import BadCharacter
+from heuristics.goodsuffix import GoodSuffix
+from performance.performanceanalyser import PerformanceAnalyser
 
 if __name__ == '__main__':
-    pa = PerformanceAnalyser()
+    pa = PerformanceAnalyser("ATGATG", "/home/aviator/Desktop/cfa_ref_CanFam3.1_chr1.fa")
     pa.add_heuristic(BadCharacter())
     pa.add_heuristic(GoodSuffix())
     pa.add_heuristic(CompositeHeuristic(GoodSuffix(),BadCharacter()))
-    pa.analyse("asdjasdvhajsdvajsdbhasjd asdbha sjasdjasvbdjas", "jasdj")
-    pa.analyse("asdjasdvhajsdvajsdbhasjd asdbha sjasdjasvbdjas", "asff")
+    pa.analyse()
     pa.show_results()
