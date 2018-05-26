8010698c21207a9957d53dbc0899c03a6aa6aa64
Lazar Beloica <lazarbeloica@gmail.com>
2018-05-26 10:16:46 +0200

    Enabled linfeeder secheur integration tests

--- src/tests/integrationtests/test_linefeeder_secondheur.py
+++ src/tests/integrationtests/test_linefeeder_secondheur.py
@@ -1,7 +1,7 @@
 from src.tests.integrationtests import test_linefeeder
 from src.heuristics.secondheuristic import SecndHeuristic
 
-#class TestLineFeederBadCharacter(test_linefeeder.TestLineFeeder):
-#
-#    def setUp(self):
-#        self.set_heuristic(SecndHeuristic())
+class TestLineFeederBadCharacter(test_linefeeder.TestLineFeeder):
+
+    def setUp(self):
+        self.set_heuristic(SecndHeuristic())
